5e8a57876618cb2b3ac865367155f6bb9f1c0f9c
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-11-25 10:35:31 -0700

    Remove createSceneFromTemplateFile

--- client/index.ts
+++ client/index.ts
@@ -1,17 +1,6 @@
-import { Matrix4, ColorMaterial } from '..';
-
 export * from './utils';
 export * from './vertex-client';
 export * from './helpers';
-
-export interface SceneTemplateItem {
-  materialOverride?: ColorMaterial;
-  name?: string;
-  parentId?: string;
-  source?: string;
-  suppliedId: string;
-  transform?: Matrix4;
-}
 
 export type BasePath =
   | 'https://platform.platdev.vertexvis.io'
