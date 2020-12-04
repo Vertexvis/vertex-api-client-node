cf381fa93d064685cea1a15a5e8ca7005d1099a3
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2020-12-03 17:26:40 -0700

    [API-1460]: Update to latest spec (#36)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-set -e
+set -o errexit -o nounset
 
-# curl -s https://platform.platdev.vertexvis.io/spec > ./spec.yml
-cp ../vertex-api/src/universal/api-resolved-public.yml ./spec.yml
+yarn clean
+
+curl -s https://platform.platdev.vertexvis.io/spec > ./spec.yml
+# cp ../vertex-api/src/universal/api-resolved-public.yml ./spec.yml
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:latest generate \
     --input-spec /local/spec.yml \
     --generator-name typescript-axios \
     --config /local/config.yml \
     --output /local
+
+sed -i "" "s/, COLLECTION_FORMATS, /, /" api.ts
+
+yarn format
