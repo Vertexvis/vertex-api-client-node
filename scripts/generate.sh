dc6594c60a2eec377910de9385889d8d48e510d9
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-01-26 16:03:45 -0700

    v0.6.7

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset
 
 yarn clean
 
-curl -s https://platform.platdev.vertexvis.io/spec > ./spec.yml
+curl -s https://platform.vertexvis.com/spec > ./spec.yml
 # cp ../vertex-api/src/universal/api-resolved-public.yml ./spec.yml
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli:latest generate \
