7c86f08f0aa00ebc6c52ed1e62fe1e02bc42d19f
Jeff Murray <jeff.murray@vertexvis.com>
2025-12-09 15:44:11 -0600

    Remove unnecessary helper

--- client/utils.ts
+++ client/utils.ts
@@ -5,7 +5,6 @@ import { parse, ParsedUrlQuery } from 'querystring';
 import { DUMMY_BASE_URL } from '../common';
 import {
   ApiError,
-  CreateSceneItemRequestData,
   Failure,
   ImageType,
   Matrix4,
@@ -287,13 +286,6 @@ export function isQueuedJob(obj: unknown): obj is QueuedJob {
     defined(qj.data?.type) &&
     qj.data.type.startsWith('queued-')
   );
-}
-
-export function isCreateSceneItemRequestData(
-  obj: unknown
-): obj is CreateSceneItemRequestData {
-  const rd = obj as CreateSceneItemRequestData;
-  return defined(rd.attributes), defined(rd.relationships), defined(rd.type);
 }
 
 export function hasVertexError(error: unknown): error is VertexError {
